373efe5dc9ae93d8d1da8042b59532e0bb9c7623
Brian Valette <47712216+brian-xu-vlt@users.noreply.github.com>
2025-05-11 11:52:46 +0200

    few fixes + scripts addition

--- examples/src/utils.tsx
+++ examples/src/utils.tsx
@@ -4,6 +4,4 @@ import { twMerge } from 'tailwind-merge';
  * Helper function to merge Tailwind CSS classes
  * This is the function used by the ts-plugin-tailwind-refactor plugin
  */
-export function tailwindMerge(...classes: string[]) {
-  return twMerge(...classes);
-}
+export const tailwindMerge = twMerge;
